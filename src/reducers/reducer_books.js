6dd923dd4369e816fc565f274be77e476c584abf
RyanCloudFM <r.wolton@cloudfmgroup.com>
2018-03-16 09:50:48 +0000

    Initial render state and book detail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'JS'},
-    {title: 'Harry P'},
-    {title: 'Some Book'},
-    {title: 'Game of Thrones'}
+    {title: 'JS', pages: 101},
+    { title: 'Harry P', pages: 400},
+    { title: 'Some Book', pages: 87},
+    { title: 'Game of Thrones', pages: 500}
   ]
 }
