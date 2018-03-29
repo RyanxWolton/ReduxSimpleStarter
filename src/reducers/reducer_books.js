3de5a800781dd07aece63f39b375c275be9c787b
RyanCloudFM <r.wolton@cloudfmgroup.com>
2018-03-29 11:25:43 +0100

    restart

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    {title: 'JS', pages: 101},
-    { title: 'Harry P', pages: 400},
-    { title: 'Some Book', pages: 87},
-    { title: 'Game of Thrones', pages: 500}
-  ]
-}
